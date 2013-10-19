289ca02061db1dac9c9f4e3a7263dd6a54f700bd
Ernest Micklei <ernest.micklei@gmail.com>
2013-10-19 08:01:19 +0200

    last minute changes

--- 2013/devfest/pointer_receiver.go
+++ 2013/devfest/pointer_receiver.go
@@ -5,23 +5,19 @@ import (
 )
 
 // START OMIT
-type Counter struct{ value int }
+type Counter struct {
+	value int
+}
 
-func (c *Counter) inc() { c.value++ }
-
-func increment(c Counter) { c.inc() }
-
-func decrement(c *Counter) { c.value-- }
+func increment(c *Counter) {
+	c.value++
+}
 
 func main() {
 	c := Counter{0}
-	c.inc()
 	fmt.Println(c.value)
 
-	increment(c)
-	fmt.Println(c.value)
-
-	decrement(&c)
+	increment(&c)
 	fmt.Println(c.value)
 }
 
