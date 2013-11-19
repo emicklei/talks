8ef77e6d13c247df9607a6fb19777effa3b0e626
Ernest Micklei <ernest.micklei@gmail.com>
2013-11-19 10:17:37 +0100

    step5 added

---
+++ 2013/meetup/step5_test.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+	"math"
+	"testing"
+)
+
+// START OMIT
+const DegToRad = math.Pi / 180
+
+func TestComputeSpiro(t *testing.T) {
+	for g := 0; g < 360; g += 10 {
+		x, y := computeSpiro(float64(g)*DegToRad, 10, 100, 2)
+		fmt.Printf("(%v,%v)\n", x, y)
+	}
+}
+
+func computeSpiro(t float64, r float64, R float64, offset float64) (x, y float64) {
+	k := r / R
+	l := offset / r
+	x1 := (1 - k) * math.Cos(t)
+	x2 := l * k * math.Cos(((1-k)/k)*t)
+	x = R * (x1 + x2)
+	y1 := (1 - k) * math.Sin(t)
+	y2 := l * k * math.Sin(((1-k)/k)*t)
+	y = R * (y1 - y2)
+	return
+}
+
+// END OMIT
