a63f3bf3b998d0cd8ea23cc3d370556b99f7620f
Ernest Micklei <>
2017-11-02 22:20:47 +0100

    start with hazana

---
+++ 2017/hazana/make.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"os"
+	"strings"
+)
+
+var slides = `
+intro.slide
+grpc.slide
+`
+
+/**
+server
+client
+loadprofile
+attacker
+hazana reporting
+run in the clouds
+hazana docker
+**/
+
+func main() {
+	writer, _ := os.Create("basic.slide")
+	scanner := bufio.NewScanner(strings.NewReader(slides))
+	for scanner.Scan() {
+		if line := scanner.Text(); len(line) > 0 {
+			include(writer, line)
+		}
+	}
+	writer.Close()
+}
+
+func include(w io.Writer, name string) {
+	fmt.Println("including", name)
+	file, err := os.Open(strings.Trim(name, " "))
+	if err != nil {
+		panic(err.Error())
+	}
+	defer file.Close()
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		if line := scanner.Text(); strings.Trim(line, " ") != "T" {
+			io.WriteString(w, line)
+			io.WriteString(w, "\n")
+		}
+	}
+}
