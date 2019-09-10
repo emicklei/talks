a2d498a54073cb5dc32679e94016cc1d096365bc
emicklei_kramphub <ernest@kramphub.com>
2019-09-10 22:36:24 +0200

    work in graphql server talk 1

---
+++ 2019/graphql/main.go
@@ -0,0 +1,69 @@
+package main
+
+// https://github.com/radovskyb/watcher/tree/master/cmd/watcher
+// watcher -cmd="go run main.go"
+
+import (
+	"bufio"
+	"flag"
+	"fmt"
+	"io"
+	"log"
+	"os"
+	"strings"
+)
+
+var debug = flag.Bool("d", false, "if true then write debug info")
+
+var slides = `
+intro.slide
+
+architecture.slide
+
+graphql.slide
+
+schema.slide
+
+query.slide
+
+principles.slide
+
+package.slide
+
+selection.slide
+`
+
+var slideCount = 0
+
+func main() {
+	flag.Parse()
+	writer, _ := os.Create("main.slide")
+	scanner := bufio.NewScanner(strings.NewReader(slides))
+	for scanner.Scan() {
+		if line := scanner.Text(); len(line) > 0 {
+			slideCount++
+			include(writer, line)
+		}
+	}
+	writer.Close()
+}
+
+func include(w io.Writer, name string) {
+	fmt.Println("including", name)
+	file, err := os.Open(strings.Trim(name, " "))
+	if err != nil {
+		log.Println(err.Error())
+		return
+	}
+	defer file.Close()
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		if line := scanner.Text(); strings.Trim(line, " ") != "T" {
+			io.WriteString(w, line)
+			io.WriteString(w, "\n")
+		}
+	}
+	if *debug {
+		io.WriteString(w, fmt.Sprintf("_%s_ %d\n\n", name, slideCount))
+	}
+}
