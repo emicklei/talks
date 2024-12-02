a5883a7c510c50e05e268c6d703cbd302fc24dca
ernest micklei <ernest.micklei@gmail.com>
2024-12-02 09:28:22 +0100

    add navara workshop slide

---
+++ 2024/nsp/workshop/nu.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"encoding/xml"
+	"fmt"
+	"log"
+	"net/http"
+)
+
+// START OMIT
+type RSS struct {
+	Channel struct {
+		Item []struct {
+			Title string `xml:"title"`
+		} `xml:"item"`
+	} `xml:"channel"`
+}
+
+type ArticleTitles struct {
+	Title []string `xml:"channel>item>title"`
+}
+
+func main() {
+	resp, err := http.Get("http://www.nu.nl/rss/Algemeen")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer resp.Body.Close()
+	rss := new(RSS)
+	if err := xml.NewDecoder(resp.Body).Decode(&rss); err != nil {
+		log.Fatal(err)
+	}
+	for _, each := range rss.Channel.Item {
+		fmt.Println(each.Title)
+	}
+}
+
+// END OMIT
