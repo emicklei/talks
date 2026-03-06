dd5a80086e057036d6e8f4499cafcb929223b3d3
emicklei <ernest.micklei@gmail.com>
2026-03-06 06:11:00 +0100

    update run.sh comment

--- 2026/gi-meetup/run.sh
+++ 2026/gi-meetup/run.sh
@@ -1,7 +1,11 @@
  #  sudo npm install -g @marp-team/marp-cli
  #  go install github.com/radovskyb/watcher/cmd/watcher@latest
  #
+ # In markdownmerge folder:
+ #
+ #     go install
+ #
  # In slides folder:
  #
- #      watcher -cmd="go run main.go -o ../main.md"
+ #     watcher -cmd="markdownmerge -i  slides.txt -o ../main.md"
  PORT=8484 marp --config marp.yml -s .
